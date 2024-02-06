f119e89cb915ed0e0b3f35dee75dea7def706726
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-02-06 14:36:14 -0300

    feature: Criado a classe e as listas

---
+++ TreinoListasCSharp/TreinoListasCSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TreinoListasCSharp
+{
+    class Program
+    {
+        public class Aluno
+        {
+            public double Nota { get; set; }
+            public string Nome { get; set; }
+            public int Idade { get; set; }
+
+            public Aluno(string nome, int idade, double nota)
+            {
+                this.Nome = nome;
+                this.Idade = idade;
+                this.Nota = nota;
+            }
+        }
+
+        List<Aluno> alunos = new()
+        {
+            new Aluno("João", 32, 9.0),
+            new Aluno("Maria", 21, 8.0),
+            new Aluno("Eduardo", 19, 4.5),
+            new Aluno("Gabriel", 25, 3.5),
+            new Aluno("Ana", 30, 2.5),
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
